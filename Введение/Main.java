be22c650c05c4fc084b0926b39bb7990903e15dc
Khaimin Dmitry <dkhaiminl@gmail.com>
2026-02-14 12:35:35 +0300

    Введение

---
+++ Введение/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in,"CP866");
+        System.out.print("Введите своё имя: ");
+        String name = scan.next();
+        System.out.println("Привет," + name);
+        scan.close();
+    }
+}
